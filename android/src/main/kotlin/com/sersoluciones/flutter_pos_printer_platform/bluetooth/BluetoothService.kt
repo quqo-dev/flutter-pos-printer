099db3f78fdcbd63fc717fc785e694e599c4afac
Johnny Luu <ngocsangluu58@gmail.com>
2026-07-13 17:33:55 +0700

    android upgrade

--- android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/bluetooth/BluetoothService.kt
+++ android/src/main/kotlin/com/sersoluciones/flutter_pos_printer_platform/bluetooth/BluetoothService.kt
@@ -72,7 +72,7 @@ class BluetoothService(private var bluetoothHandler: Handler?) {
     // Scan ble
     ////////////////////////////////////////////////////////////////////////////////////////////////
     fun scanBleDevice(mChannel: MethodChannel) {
-        if (bleScanner == null) return
+        val scanner = bleScanner ?: return
         devicesBle.clear()
         handler.removeCallbacksAndMessages(null)
         // Device scan callback.
@@ -83,7 +83,7 @@ class BluetoothService(private var bluetoothHandler: Handler?) {
         if (!scanning) { // Stops scanning after a pre-defined scan period.
             handler.postDelayed({
                 scanning = false
-                bleScanner.stopScan(leScanCallback)
+                scanner.stopScan(leScanCallback)
                 bluetoothHandler?.obtainMessage(BluetoothConstants.MESSAGE_STOP_SCANNING, -1, -1)
                     ?.sendToTarget()
                 Log.d(TAG, "----- stop scanning ble ------- ")
@@ -96,12 +96,12 @@ class BluetoothService(private var bluetoothHandler: Handler?) {
             }, SCAN_PERIOD)
             Log.d(TAG, "----- start scanning ble ------ ")
             scanning = true
-            bleScanner.startScan(leScanCallback)
+            scanner.startScan(leScanCallback)
             bluetoothHandler?.obtainMessage(BluetoothConstants.MESSAGE_START_SCANNING, -1, -1)
                 ?.sendToTarget()
         } else {
             scanning = false
-            bleScanner.stopScan(leScanCallback)
+            scanner.stopScan(leScanCallback)
             bluetoothHandler?.obtainMessage(BluetoothConstants.MESSAGE_STOP_SCANNING, -1, -1)
                 ?.sendToTarget()
         }
@@ -218,7 +218,7 @@ class BluetoothService(private var bluetoothHandler: Handler?) {
         if (!adapter.isEnabled) {
             adapter.enable()
             while (true) {
-                if (mBluetoothAdapter.isEnabled) break
+                if (adapter.isEnabled) break
             }
             return
         }
